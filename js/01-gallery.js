02b9b87f5e8e9e048b6b62b98637c5970611d075
Fedoriaka <145745841+Fedoriaka@users.noreply.github.com>
2024-08-31 23:13:57 +0300

    hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const container = document.querySelector(".gallery");
+const gallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `  <li class="gallery__item">
+      <a href="${original}" class="gallery__link">
+        <img class="gallery__image" src="${preview}" data-source = "${original}" alt="${description}" />
+      </a>
+    </li>
+  `;
+  })
+  .join("");
+container.innerHTML = gallery;
+
+container.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    const largeImage = event.target.dataset.source;
+
+    const instance = basicLightbox.create(
+      `
+    <img src="${largeImage}"  width="1400" height="900">
+`,
+      {
+        onShow: (instance) => {
+          document.addEventListener("keydown", onEscKeyPress);
+        },
+        onClose: (instance) => {
+          document.removeEventListener("keydown", onEscKeyPress);
+        },
+      }
+    );
+
+    instance.show();
+
+    function onEscKeyPress(event) {
+      if (event.key === "Escape") {
+        instance.close();
+      }
+    }
+});
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const container = document.querySelector(".gallery");
+const gallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `  <li class="gallery__item">
+      <a href="${original}" class="gallery__link">
+        <img class="gallery__image" src="${preview}"  alt="${description}" />
+      </a>
+    </li>
+  `;
+  })
+  .join("");
+container.innerHTML = gallery;
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt", 
+  captionDelay: 250,
+});
 console.log(galleryItems);
